78a6f7a72c9b5d052c5c8c414d2082dba4dcdb29
unoseistres <sharondelacruz@gmail.com>
2015-03-26 12:19:48 -0400

    RESIZABLE WORKS
    not the most intuitive for touch but good for low version prototype

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/dickweed/documents/app_4/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/dickweed/documents/app_4/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,10 @@
+/Users/dickweed/documents/app_4/platforms/android/ant-build/classes.dex : \
+/Users/dickweed/documents/app_4/platforms/android/ant-build/classes/com/phonegap/helloworld/BuildConfig.class \
+/Users/dickweed/documents/app_4/platforms/android/ant-build/classes/com/phonegap/helloworld/CordovaApp.class \
+/Users/dickweed/documents/app_4/platforms/android/ant-build/classes/com/phonegap/helloworld/R$attr.class \
+/Users/dickweed/documents/app_4/platforms/android/ant-build/classes/com/phonegap/helloworld/R$drawable.class \
+/Users/dickweed/documents/app_4/platforms/android/ant-build/classes/com/phonegap/helloworld/R$string.class \
+/Users/dickweed/documents/app_4/platforms/android/ant-build/classes/com/phonegap/helloworld/R$xml.class \
+/Users/dickweed/documents/app_4/platforms/android/ant-build/classes/com/phonegap/helloworld/R.class \
+/Users/dickweed/documents/app_4/platforms/android/ant-build/classes/org/devgeeks/Canvas2ImagePlugin/Canvas2ImagePlugin.class \
+/Users/dickweed/documents/app_4/platforms/android/ant-build/dexedLibs/classes-0bb6509681cdb6c59aecc40a68776526.jar \
